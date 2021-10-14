b9132f1163d977a758e7b6720e077558ee9f3bed
o0juano0o <juam_92@hotmail.com>
2021-10-14 00:08:32 -0300

    subida podcast terminada

--- app/utils/transcription.js
+++ app/utils/transcription.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 const API = 'http://10.0.2.2:3001';
 
 export const getTranscription = audio => {
-  console.log(audio);
+  //console.log(audio);
   return axios
     .post(`http://10.0.2.2:3001/api/transcription`, audio)
     .then(res => {
-      console.log(res.data);
+      // console.log(res.data);
       return res.data;
     })
     .catch(err => {
